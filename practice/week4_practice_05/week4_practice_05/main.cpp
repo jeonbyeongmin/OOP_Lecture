9769f77178438d048e69188f75607d296fb4b06d
jeonbyeongmin <qudals7613@gmail.com>
2020-09-21 09:47:17 +0900

    week4_practice_05

---
+++ practice/week4_practice_05/week4_practice_05/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  week4_practice_05
+//
+//  Created by 전병민 on 2020/09/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    const int &x = 5;
+    std::cout<< x << std::endl;
+    return 0;
+}
