7c305b5d3ab6e7302e4c20f9321686f670092e82
jeonbyeongmin <qudals7613@gmail.com>
2020-09-22 02:48:33 +0900

    week04

---
+++ homework/week04/week04_homework_01/week04_homework_01/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  week04_homework_01
+//
+//  Created by 전병민 on 2020/09/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+
+    char str[300];
+    printf("문자를 입력하세요(100자 이내). \n");
+    scanf("%s", str);
+    printf("입력된 문자는 %s입니다 \n", str);
+    return 0;
+    
+}
---
+++ homework/week04/week04_homework_02/week04_homework_02/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  week04_homework_02
+//
+//  Created by 전병민 on 2020/09/22.
+//
+
+#include <iostream>
+void swap(int &first, int &second)
+{
+    int temp = first;
+    first = second;
+    second = temp;
+}
+
+int main(int argc, const char * argv[]) {
+    int a = 2;
+    int b = 3;
+    swap(a, b);
+    
+    std::cout << "a = " << a << ", " << "b = " << b << std::endl;
+    return 0;
+}
